c994dc06297b5ad635182417c3e8383110869452
albimerlyn-crypto <albimerlyn@gmail.com>
2026-03-09 15:29:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,63 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+let usuarios = [];
 let productos = [];
-let noticias = [];
 
-app.get("/productos", (req,res)=>{
-res.json(productos);
-});
+app.post("/registro",(req,res)=>{
 
-app.post("/productos", (req,res)=>{
-const nuevo = req.body;
-productos.push(nuevo);
-res.json({ok:true});
-});
+const usuario=req.body.usuario
+const password=req.body.password
 
-app.get("/noticias",(req,res)=>{
-res.json(noticias);
-});
+usuarios.push({usuario,password})
 
-app.post("/noticias",(req,res)=>{
-noticias.push(req.body);
-res.json({ok:true});
-});
+res.json({mensaje:"usuario creado"})
+
+})
+
+app.post("/login",(req,res)=>{
+
+const usuario=req.body.usuario
+const password=req.body.password
+
+const encontrado = usuarios.find(
+u=>u.usuario===usuario && u.password===password
+)
+
+if(encontrado){
+
+res.json({login:true})
+
+}else{
+
+res.json({login:false})
+
+}
+
+})
+
+app.get("/productos",(req,res)=>{
+
+res.json(productos)
+
+})
+
+app.post("/productos",(req,res)=>{
+
+productos.push(req.body)
+
+res.json({ok:true})
+
+})
 
 app.listen(3000,()=>{
-console.log("Servidor iniciado");
-});
+
+console.log("Servidor iniciado")
+
+})
